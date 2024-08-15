a31117eb173f98b0fab756de14b543e4154e1a2c
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-08-15 13:17:03 +0300

    a

--- CustomModules/Voidware6872274481.lua
+++ CustomModules/Voidware6872274481.lua
@@ -75,18 +75,23 @@ GuiLibrary.SelfDestructEvent.Event:Connect(function()
 end)
 
 task.spawn(function()
-	pcall(function()
-		local lplr = game:GetService("Players").LocalPlayer
-		local char = lplr.Character or lplr.CharacterAdded:wait()
-		local displayName = char:WaitForChild("Head"):WaitForChild("Nametag"):WaitForChild("DisplayNameContainer"):WaitForChild("DisplayName")
-		repeat task.wait() until shared.vapewhitelist
-		repeat task.wait() until shared.vapewhitelist.loaded
-		local tag = shared.vapewhitelist:tag(lplr, "", true)
-		if displayName.ClassName == "TextLabel" then
-			if not displayName.RichText then displayName.RichText = true end
-			displayName.Text = tag..lplr.Name
-		end
-	end)
+    pcall(function()
+        local lplr = game:GetService("Players").LocalPlayer
+        local char = lplr.Character or lplr.CharacterAdded:wait()
+        local displayName = char:WaitForChild("Head"):WaitForChild("Nametag"):WaitForChild("DisplayNameContainer"):WaitForChild("DisplayName")
+        repeat task.wait() until shared.vapewhitelist
+        repeat task.wait() until shared.vapewhitelist.loaded
+        local tag = shared.vapewhitelist:tag(lplr, "", true)
+        if displayName.ClassName == "TextLabel" then
+            if not displayName.RichText then displayName.RichText = true end
+            displayName.Text = tag..lplr.Name
+        end
+        displayName:GetPropertyChangedSignal("Text"):Connect(function()
+            if displayName.Text ~= tag..lplr.Name then
+                displayName.Text = tag..lplr.Name
+            end
+        end)
+    end)
 end)
 
 local GetEnumItems = function() return {} end
