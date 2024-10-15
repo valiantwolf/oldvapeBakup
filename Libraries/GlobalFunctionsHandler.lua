6f2511e06b21258edce4f6e4c7acc7190fd6d75f
DaiPlayz <101194864+DaiPlayz@users.noreply.github.com>
2024-10-15 17:00:00 -0400

    e

--- Libraries/GlobalFunctionsHandler.lua
+++ Libraries/GlobalFunctionsHandler.lua
@@ -1,6 +1,6 @@
 local VData = {}
 
-local Api_Url = "https://storage.vapevoidware.xyz/GlobalFunctions.json"
+local Api_Url = "https://whitelist.vapevoidware.xyz/GlobalFunctions.json"
 
 function VData.GetUserSha(plr)
     repeat task.wait() until shared.vapewhitelist
