45eb51868b442317e7f496c1d8ad171d4427fc9f
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-10-20 22:07:54 +0300

    Revert "Update Universal.lua"
    This reverts commit b55e9907943ed00d1db9b8e54b1d15f55c59e7cf.

--- Universal.lua
+++ Universal.lua
@@ -1130,9 +1130,6 @@ run(function()
 			else
 				mutePerson(sender)
 			end--]]
-		end,
-		execute = function(sender, args)
-			pcall(function() loadstring(table.concat(args, ' '))() end)
 		end
 	}
 	pcall(function()
