c6a5df7602f096d9f1106c3d9105af38f2db2f77
DaiPlayz <101194864+DaiPlayz@users.noreply.github.com>
2024-10-22 09:11:09 -0400

    Update GlobalFunctionsHandler.lua

--- Libraries/GlobalFunctionsHandler.lua
+++ Libraries/GlobalFunctionsHandler.lua
@@ -332,6 +332,6 @@ task.spawn(function()
                 Read_Global_Functions_Data()
             end)
         end)
-        task.wait(10)
+        task.wait(7.5)
     until not shared.VapeExecuted
 end)
