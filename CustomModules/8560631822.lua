bffafd357398e23f5ce9f1d12292cbe74004b54d
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-10-29 16:42:30 +0200

    Update 8560631822.lua

--- CustomModules/8560631822.lua
+++ CustomModules/8560631822.lua
@@ -1,20 +1,3 @@
---This watermark is used to delete the file if its cached, remove it to make the file persist after commits.
-local function vapeGithubRequest(scripturl)
-	if not isfile("vape/"..scripturl) then
-		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/"..readfile("vape/commithash.txt").."/"..scripturl, true) end)
-		if not suc or res == "404: Not Found" then return nil end
-		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
-		writefile("vape/"..scripturl, res)
-	end
-	return readfile("vape/"..scripturl)
-end
-
+local pload = shared.pload or function(file, isImportant) local suc, err = pcall(function() loadstring(readfile('vape/'..tostring(file))) end) if not suc and err then local text = "[Voidware Error] Error loading critical file! FileName: "..tostring(file).." Error: "..tostring(err) warn(text) if isImportant then game:GetService("Players").LocalPlayer:Kick(text) end end end
 shared.CustomSaveVape = 6872274481
-if isfile("vape/CustomModules/6872274481.lua") then
-	loadstring(readfile("vape/CustomModules/6872274481.lua"))()
-else
-	local publicrepo = vapeGithubRequest("CustomModules/6872274481.lua")
-	if publicrepo then
-		loadstring(publicrepo)()
-	end
-end
+pload("CustomModules/6872274481.lua")
