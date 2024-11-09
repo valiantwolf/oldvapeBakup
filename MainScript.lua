2b2552c9698967b4edde6b506fe527017dd23161
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-11-09 17:08:34 +0200

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -2222,7 +2222,7 @@ local function loadVape()
 			fileName1 = "CustomModules/"..CE.."6872265039.lua"
 			fileName2 = "CustomModules/VW6872265039.lua"
 		end
-		if CE == "CE" then InfoNotification("Voidware", "Backup mode activated!", 3) end 
+		--if CE == "CE" then InfoNotification("Voidware", "Backup mode activated!", 3) end 
 		--if shared.CheatEngineMode then InfoNotification(fileName1, fileName2, 2) end
 		if shared.VoidDev then InfoNotification(fileName1, fileName2, 100) end
 		pload(fileName1, true)
