4206e9795881846cabce03f43ef5773b0337e422
Erchobg <67239055+Erchobg@users.noreply.github.com>
2024-12-15 12:26:06 +0200

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -396,7 +396,6 @@ local function vapeGithubRequest(scripturl, isImportant)
     end
     local suc, res
     local url = (scripturl == "MainScript.lua" or scripturl == "GuiLibrary.lua") and shared.RiseMode and "https://raw.githubusercontent.com/VapeVoidware/VWRise/" or "https://raw.githubusercontent.com/VapeVoidware/VoidwareBakup/"
-    print(url..readfile(baseDirectory.."commithash2.txt").."/"..scripturl)
     suc, res = pcall(function() return game:HttpGet(url..readfile(baseDirectory.."commithash2.txt").."/"..scripturl, true) end)
     if not suc or res == "404: Not Found" then
         if isImportant then
