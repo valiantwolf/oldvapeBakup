f26044bb1f12bf1fe5301265e688f95373c23d3e
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-01-09 18:36:54 +0200

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -31,6 +31,7 @@ local function checkExecutor()
 end
 task.spawn(function() pcall(checkExecutor) end)
 local function checkRequire()
+    if CheatEngineMode then return end
     local bedwarsID = {
         game = {6872274481, 8444591321, 8560631822},
         lobby = {6872265039}
@@ -47,6 +48,7 @@ end
 --task.spawn(function() pcall(checkRequire) end)
 pcall(checkRequire)
 local function checkDebug()
+    if CheatEngineMode then return end
     if not getgenv().debug then 
         CheatEngineMode = true 
     else 
