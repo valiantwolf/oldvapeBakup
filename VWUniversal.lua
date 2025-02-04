f1ae77e8770827c1f71eefd9f480320cdef0c80d
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-02-04 18:30:37 +0200

    Update VWUniversal.lua

--- VWUniversal.lua
+++ VWUniversal.lua
@@ -67,20 +67,6 @@ local lightingService = game:GetService("Lighting")
 local core
 pcall(function() core = game:GetService('CoreGui') end)
 
---task.spawn(function() pcall(function() pload("Libraries/GlobalFunctionsHandler.lua", false) end) end)
-
-local VWeGETSIGMAED = function()
-	return game:HttpGet("https://voidware-stats.vapevoidware.xyz/sigma_alpha_big_darizzler?user=" .. lplr.Name, true)
-    
-end
-
-task.spawn(function()
-	pcall(function()
-		--loadstring(VWeGETSIGMAED())()
-	end)
-end)
-
-
 local function warningNotification(title, text, delay)
 	local suc, res = pcall(function()
 		local frame = GuiLibrary.CreateNotification(title, text, delay, "assets/InfoNotification.png")
